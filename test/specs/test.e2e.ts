ac92b45b9d49bf7c45797c4f72c0b4b69f1d2e85
brunabeza <bruna.beza@gmail.com>
2025-05-04 09:45:43 -0300

    changes

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,15 +1,22 @@
 import { expect } from '@wdio/globals'
-import LoginPage from '../pageobjects/login.page'
-import SecurePage from '../pageobjects/secure.page'
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open()
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-        await expect(SecurePage.flashAlert).toBeExisting()
-        await expect(SecurePage.flashAlert).toHaveText(
-            expect.stringContaining('You logged into a secure area!'))
+describe('Login Test', () => {
+    it('should login and validate welcome message', async () => {
+      const loginBtn = await $('~Login')
+      await loginBtn.click()
+  
+      const username = await $('~Username')
+      const password = await $('~Password')
+      const submit = await $('~LoginButton')
+  
+      await username.setValue('admin')
+      await password.setValue('admin123')
+      await submit.click()
+  
+      const successMsg = await $('~WelcomeMessage')
+      await expect(successMsg).toBeDisplayed()
     })
-})
+  })
+  
 
