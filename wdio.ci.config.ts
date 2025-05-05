d057f3ddf8bdea6baa383f4ba387fae6f9e0e678
brunabeza <bruna.beza@gmail.com>
2025-05-05 15:03:54 -0300

    bS

--- wdio.ci.config.ts
+++ wdio.ci.config.ts
@@ -1,32 +1,26 @@
-import { Config } from '@wdio/types';
 
-export const config: Config = {
-    runner: 'local',
-    specs: ['./test/specs/**/*.ts'],
-    maxInstances: 1,
-
+exports.config = {
+    user: process.env.BROWSERSTACK_USERNAME,  // Use a variável de ambiente configurada no GitHub
+    key: process.env.BROWSERSTACK_ACCESS_KEY,  // Use a variável de ambiente configurada no GitHub
+    services: [
+      ['browserstack', {
+        browserstackLocal: true  // Permite usar BrowserStack Local, caso necessário
+      }]
+    ],
     capabilities: [{
-        platformName: 'Android',
-        'appium:deviceName': 'emulator-5554',
-        'appium:platformVersion': '14',
-        'appium:automationName': 'UiAutomator2',
-        'appium:app': './apps/alura_esporte.apk',
-        'appium:autoGrantPermissions': true
+      browserName: 'Android',
+      platformName: 'Android',
+      device: 'Google Pixel 4',  // Nome do dispositivo que você quer emular
+      os_version: '10.0',  // Versão do Android
+      realMobile: true,  // Usando dispositivo real
+      app: 'bs://<app-id>',  // Substitua <app-id> pelo ID do seu app carregado no BrowserStack
+      automationName: 'UiAutomator2'  // O nome da automação para dispositivos Android
     }],
 
-    services: [
-        ['appium', { 
-            args: { 
-                "--relaxed-security": true 
-            },
-            command: 'appium'
-        }]
-    ],
-
     framework: 'mocha',
-    reporters: ['spec', ['allure', { outputDir: 'allure-results' }]],
+    reporters: ['spec', ['allure', { outputDir: 'allure-results' }]],  // Relatórios usando Allure
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: 60000  // Tempo limite para os testes
     }
-};
+}
